98532a2ba48c11f06241ffef89325b13bfaabb21
Prismetric Technologies Pvt Ltd <info@prismetric.com>
2023-12-13 14:45:00 +0530

    Uploading code for the CICD demo

---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="home">
+        <div class = "navbar">
+            <t>foodiv</t>
+        </div>
+        <Section1 />
+        <Section2 />
+        </div>
+    </template>
+<script>
+import Section1 from "./Section1.vue"
+import Section2 from "./Section2.vue"
+export default {
+  name: 'HomePage',
+  components:{
+    Section1,
+    Section2,
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar{
+    width: 20%;
+    align-self: self-start;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+t {
+  color: rgb(247,99,48);
+  font-size: 350%;
+  font-weight: 900; 
+  margin-top: 0%;
+}
+</style>
---
+++ src/components/Section1.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="section">
+       <div class = "information">
+        <ul>
+            <t>Get your own online Food Ordering App and Website</t>
+        </ul>
+        <ul>
+            <p> Integrate the food ordering system in your restaurant 
+                and grab the future. our restaurant online ordering software is 
+                capable enough to serve your purposes successfully. 
+            </p>
+            <ul>No commision</ul>
+            <ul>More control on customer data</ul>
+            <ul>Free QR Code Menu</ul>
+            <ul>Free Oneli ordering system</ul>
+        </ul>
+       </div>
+       <div class = "imagesec">
+        <img src="../assets/imgs/s1img.jpg" alt="Your Image Alt Text" />
+       </div>
+    </div>
+    </template>
+<script>
+export default {
+  name: 'SectionStyle'
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.section{
+    display: flex;
+}
+.information{
+    width: 55%;
+    height: 500px;
+}
+.imagesec{
+    width: 45%;
+    height: 500px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 15px;
+  text-align: left;
+}
+li {
+  display: inline-block;
+  margin: 15px;
+  text-align: left;
+}
+p {
+  display: inline-block;
+  margin: 15px;
+  text-align: left;
+}
+t {
+  color: black;
+  font-size: 250%;
+  font-weight: 600;
+  text-shadow: 0 10px 8px rgba(0, 0, 0, 0.1); /* Set your desired shadow properties */  
+  margin: 10px;
+}
+</style>
